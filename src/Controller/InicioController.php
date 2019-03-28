367fa69f77784c7c94027349e58f1e2fb5c430af
MarioA <maestradaz3@gmail.com>
2019-03-27 21:44:11 -0500

    Commit inicial

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -6,6 +6,8 @@ use App\Entity\Llamada;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class InicioController extends Controller
@@ -18,10 +20,35 @@ class InicioController extends Controller
 
     public function inicioAction(Request $request)
     {
-        // en index pagina con datos generales de la app
-        return $this->render('Inicio/inicio.html.twig', [
+        $arNegocios = $this->listaNegocio();
+        $form = $this->createFormBuilder()
+            ->add('txtBuscar', TextType::class)
+            ->add('btnBuscar', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('btnBuscar')->isClicked()) {
+
+            }
+        }
+        return $this->render('inicio.html.twig', [
+            'arNegocios' => $arNegocios,
+            'form' => $form->createView()
         ]);
     }
 
+    private function listaNegocio () {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST => 1,
+            CURLOPT_URL => 'http://localhost/jakoservicio/public/index.php/v1/negocio/buscar',
+            CURLOPT_POSTFIELDS => json_encode([
+                'negocio' => ''
+            ])
+        ));
+        $arNegocios = json_decode(curl_exec($curl), true);
+        return $arNegocios;
+    }
 }
